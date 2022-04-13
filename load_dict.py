92eac4aaf34803836bd79e26dc829073d2ebb052
cwzchan <cwchan0212@gmail.com>
2022-04-13 23:54:47 +0800

    Update load_dict.py

--- load_dict.py
+++ load_dict.py
@@ -2,16 +2,32 @@ dict = open("dict.txt", "r")
 
 ln = ""
 pos = -1
+codeList = []
+cnt = 0
+
+wl = ""
+nl = ""
+q1 = ""
 
 for x in dict:
     ln = x.strip()
-    #print(ln)
+
     try:
-        pos = ln.index("W=")
-        if (pos > -1):
-            print (ln)
+        if (ln.index(str(cnt+1) + ".") >=0):
+            pos = ln.index("=")
+            print (ln[pos+1:len(ln)])
+            #print(ln, pos)
+
+
     except ValueError:
         pos = -1
+    #print(ln)
+    # try:
+    #     pos = ln.index("1)
+    #     if (pos > -1):
+    #         print (ln)
+    # except ValueError:
+    #     pos = -1
 
 
     #if (ln.index("W=")> 0) :
