6d0cdd84142ab17d7a56955370041eae9a774032
cwzchan <cwchan0212@gmail.com>
2022-04-14 23:42:36 +0800

    Update load_dict.py

--- load_dict.py
+++ load_dict.py
@@ -11,16 +11,26 @@ q1 = ""
 
 for x in dict:
     ln = x.strip()
-
     try:
-        if (ln.index(str(cnt+1) + ".") >=0):
+
+        if (ln.index(str(cnt+1) + ".W") >=0):
+            print (str(cnt+1))
+            #print(1)
             pos = ln.index("=")
             print (ln[pos+1:len(ln)])
-            #print(ln, pos)
+        if (ln.index(str(cnt+1) + ".N") >= 0):
+            #print(2)
+            pos = ln.index("=")
+            print (ln[pos+1:len(ln)])
+        if (ln.index(str(cnt+1) + ".Q") >=0):
+            pos = ln.index("=")
+            print (ln[pos+1:len(ln)])
 
-
+        cnt += 1
     except ValueError:
         pos = -1
+
+
     #print(ln)
     # try:
     #     pos = ln.index("1)
