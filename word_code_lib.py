06e2f5376f2249af29cb28527027d24de3c274f4
cwzchan <cwchan0212@gmail.com>
2022-04-27 22:58:05 +0800

    Update word_code_lib.py

--- word_code_lib.py
+++ word_code_lib.py
@@ -17,16 +17,30 @@ class WordBank():
             #lnStr = ''.join(str(e) for e in p[i])
             lnStr = "".join(i)
             wStrList.append(lnStr)
+
         wnList = compare(wStrList, nlStr)
         dit = joinList2Dict(wnList)
         #print("d", dit)
         return dit
 
     def puzzle(self, dit, query):
-        if (query.isnumeric()):
+        ans = ""
+        if (isinstance(query, int)):
+            #print ("it is number!", query, dit)
+            nl = [int(i) for i in list(str(query))]
             dit = dict((v,k) for k,v in dit.items())
-            nl = [int(x) for i in list(query)]
-            print ("it is number!")
+            for k in dit:
+                for i in nl:
+                    if (i == k):
+                        ans += dit[k]
+        else:
+            #print ("it is NOT number!", query, dit)
+            nl = list(query)
+            for k in dit:
+                for i in nl:
+                    if (i == k):
+                        ans += str(dit[k])
+        return ans
 
 def joinList2Dict(wnList):
     n = w = []
@@ -34,11 +48,15 @@ def joinList2Dict(wnList):
     if wnList != []:
         w = list(wnList[0])
         n = [int(x) for x in list(wnList[1])]
+        #print("wn", w, n)
+
         for i in range(len(w)):
             dit[w[i]] = n[i]
-        dit = sorted(dit.items(), key = lambda kv:(kv[0], kv[1]))
-        #print(dit)
-
+        #for i in sorted (dit.keys()) :
+        #    print (dit[i], i)
+        sorted(dit.items(), key = lambda kv:(kv[1], kv[0]))
+        #dit = sorted(dit.items(), key = lambda kv:(kv[0], kv[1]))
+        #print("dit", dit)
     return dit
 
 def compare(wStrList, nlStr):
@@ -55,7 +73,6 @@ def compare(wStrList, nlStr):
             break
     return wnList
 
-
 def str2List(wStr):
     wl = wStr.split(" ")
     return wl
@@ -67,5 +84,6 @@ wl = wStr.split(" ")
 nl = [int(i) for i in nStr.split(" ")]
 w = WordBank()
 dit = w.solveCode(wl, nl)
-print (dit)
-w.puzzle(dit, "7354")
+#print ("out", dit)
+s = w.puzzle(dit, "TALE")
+print(s)
